778b5c11594ac12a179ec29cad4d6b4f15136a3e
ytnom00 <monuyaduvanshi808@gmail.com>
2025-03-02 03:49:14 -0800

    Update pract.cpp

--- pract.cpp
+++ pract.cpp
@@ -1,22 +1,13 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
-    int n=4;
+    vector <int> vec={1,2,3,3,2,1,4};
+    int ans=0;
+    for(int val : vec){
+        ans^=val;}
+    cout<<"the no. is"<<ans;
+    return 0;
    
-  int arr[n+2]={1,2,3,4,1,2};
-  int res[2];
-for(int i=0;i<n+2;i++){
-    for(int j=0;j<n+2;j++){
-      if(i!=j){
-        if(arr[i]==arr[j]){
-          res[0]=arr[i];
-          
-
-        }
-      }
-
-    }
-    }
-    cout<<res[0];
 }
 
