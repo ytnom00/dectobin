d59911fc9e430700c41b973a97df43241a35236b
ytnom00 <monuyaduvanshi808@gmail.com>
2025-03-02 03:44:53 -0800

    Update code.c

--- code.c
+++ code.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
+int bintodec(int bin){
+int ans=0,pow=1;
+while(bin>0){
+int rem=bin%10;
+    ans+=rem*pow;
+    bin/=10;
+    pow*=2;
+}
+return ans;
+}
 int main()
 {
-    char ch='t';
-    printf("%c",ch);
+    int bin=111;
+    printf("%d",bintodec(bin));
+    return
+    
 }
